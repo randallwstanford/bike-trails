7b85013ee110836f947e1162a276d0b4df517df7
randallwstanford <randallwstanford@gmail.com>
2022-01-29 15:49:49 -0700

    Creates header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  const headerStyle = {
+    margin: '1rem',
+  };
+
+  return (
+    <nav style={headerStyle}>
+      <h2>Bike Trails</h2>
+    </nav>
+  );
+}
+
+export default Header;
