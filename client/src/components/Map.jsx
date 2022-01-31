57374f758cbd620e35a8629a8733bae8275d5290
randallwstanford <randallwstanford@gmail.com>
2022-01-31 10:58:32 -0700

    Map now pans when city is selected.

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,66 +1,72 @@
-import React, { useState, useCallback } from 'react';
+/* eslint-disable no-new */
+/* eslint-disable react/jsx-one-expression-per-line */
+// Modules
+import React, { useCallback } from 'react';
 import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+
+// Components
+import Search from './Search';
+
+// API Key
 import { apiKey } from '../../../api-key';
+
+// Styles
+import { placesContainer, mapContainerStyle } from './MapStyles';
 
 let service;
 
-const mapContainerStyle = { width: '40rem', height: '40rem' };
-const placeStyle = { border: '1px solid black', margin: '.2rem' };
+const libraries = ['places'];
 const options = { disableDefaultUI: true, zoomControl: true };
 const center = { lat: 39.75, lng: -105.00 };
 
 function Map() {
-  const { isLoaded, loadError } = useLoadScript({ googleMapsApiKey: apiKey, libraries: ['places'] });
-  const [places, setPlaces] = useState([]);
+  const { isLoaded, loadError } = useLoadScript({ googleMapsApiKey: apiKey, libraries });
   const mapRef = React.useRef();
-  const onMapLoad = useCallback((map) => { mapRef.current = map; }, []);
 
-  const handleChange = (e) => {
-    const input = e.target.value;
+  // Map Load script
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+    const bikeLayer = new window.google.maps.BicyclingLayer();
+    bikeLayer.setMap(map);
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(12);
+    const map = mapRef.current;
+
     const request = {
-      location: center, radius: '500', query: input, type: ['restaurant'], region: ['US'],
+      location: { lat, lng },
+      radius: '500',
+      type: ['park'],
     };
 
     service = new window.google.maps.places.PlacesService(mapRef.current);
     service.nearbySearch(request, (results, status) => {
       if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-        setPlaces(results);
+        for (let i = 0; i < results.length; i += 1) {
+          const place = results[i];
+          new window.google.maps.Marker({ position: place.geometry.location, map });
+        }
       }
     });
-  };
+  }, []);
 
   if (loadError) return <div>Map cannot be loaded right now, sorry.</div>;
 
   return isLoaded
     ? (
       <div>
-        <input onChange={handleChange} />
         <GoogleMap
-          id="map"
           mapContainerStyle={mapContainerStyle}
-          zoom={8}
+          zoom={10}
           center={center}
           options={options}
           onLoad={onMapLoad}
         />
-        {
-        places.length > 0
-          ? places.slice(0, 5).map((place, index) => (
-            <div key={index} style={placeStyle}>
-              <div>
-                Name:
-                {' '}
-                {place.name}
-              </div>
-              <div>
-                Address:
-                {' '}
-                {place.vicinity}
-              </div>
-            </div>
-          ))
-          : <div>No results.</div>
-      }
+        <div style={placesContainer}>
+          <Search panTo={panTo} />
+        </div>
       </div>
     )
     : <div>Loading...</div>;
