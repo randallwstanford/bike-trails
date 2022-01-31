ed071cbac56139b4573484296976581afaff878c
randallwstanford <randallwstanford@gmail.com>
2022-01-31 11:38:23 -0700

    Moves Search component to Search component folder.

---
+++ client/src/components/Search/SearchStyles.js
@@ -0,0 +1,19 @@
+const inputContainerStyle = {
+  width: '97.5%',
+};
+const inputStyle = {
+  width: '99%',
+};
+
+const suggestStyle = {
+  textAlign: 'center', width: '100%', textDecoration: 'none', borderRadius: '0px', border: 'none',
+};
+
+const suggestionsStyle = {
+  width: '100%',
+  border: '1px solid black',
+};
+
+module.exports = {
+  suggestionsStyle, suggestStyle, inputStyle, inputContainerStyle,
+};
