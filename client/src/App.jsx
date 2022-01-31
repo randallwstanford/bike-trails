7cf4cc5740652c3ff6db119ee7a797f4686d604f
randallwstanford <randallwstanford@gmail.com>
2022-01-31 11:35:58 -0700

    Moves map and search to component folders.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import Map from './components/Map';
+import Map from './components/Map/Map';
 import Header from './components/Header';
 
-const mainStyles = {
-  height: '60vh',
-  margin: '10px 50px',
-  filter: 'drop-shadow(-1px 5px 3px #ccc)',
-};
+const appContainerStyles = { margin: '10px 50px' };
 
 function App() {
   return (
-    <div style={mainStyles}>
+    <div style={appContainerStyles}>
       <Header />
       <Map />
     </div>
