fe4fcb214e58ecb6567954e4c2fe40ecef5df343
randallwstanford <randallwstanford@gmail.com>
2022-01-31 14:39:09 -0700

    Max length increased, imports and deconstruction now inline.

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,30 +1,14 @@
-/* eslint-disable react/function-component-definition */
 /* eslint-disable no-console */
-/* eslint-disable react/prop-types */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable react/jsx-one-expression-per-line */
-import React from 'react';
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from 'use-places-autocomplete';
 
-import {
-  inputContainerStyle, inputStyle, suggestionsStyle, suggestStyle,
-} from './SearchStyles';
+import React from 'react';
+import PropTypes from 'prop-types';
+import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete';
+import { inputContainerStyle, inputStyle, suggestionsStyle, suggestStyle } from './SearchStyles';
 
 const Search = ({ panTo }) => {
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete();
+  const { value, suggestions: { status, data }, setValue, clearSuggestions } = usePlacesAutocomplete();
 
   const handleInput = (e) => setValue(e.target.value);
-
   const handleSelect = ({ description }) => () => {
     setValue(description, false);
     clearSuggestions();
@@ -44,17 +28,14 @@ const Search = ({ panTo }) => {
 
   return (
     <div style={inputContainerStyle}>
-      <input
-        value={value}
-        onChange={handleInput}
-        disabled={!ready}
-        placeholder="Where are you going?"
-        style={inputStyle}
-      />
-      {/* We can use the "status" to decide whether we should display the dropdown or not */}
+      <input value={value} onChange={handleInput} placeholder="Where are you going?" style={inputStyle} />
       {status === 'OK' ? <div style={suggestionsStyle}>{renderSuggestions()}</div> : null}
     </div>
   );
+};
+
+Search.propTypes = {
+  panTo: PropTypes.func.isRequired,
 };
 
 export default Search;
