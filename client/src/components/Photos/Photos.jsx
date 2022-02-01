0ce1eeaad819a3c1ce6f835dafa9270a9f2b2222
randallwstanford <randallwstanford@gmail.com>
2022-02-01 12:49:09 -0700

    Removed image count.

--- client/src/components/Photos/Photos.jsx
+++ client/src/components/Photos/Photos.jsx
@@ -9,14 +9,9 @@ import { photoStyles, noPhotosStyle } from './PhotosStyle';
 
 const Photos = ({ photos }) => (
   <div>
-    {console.log('photos: ', photos)}
     {photos !== undefined
-      ? (
-        <div>
-          {photos.length === 1 ? <div>Found 1 photo.</div> : <div>Found {photos.length} photos.</div>}
-          <img src={photos[0].getUrl()} alt="" height="50rem" style={photoStyles} />
-        </div>
-      ) : <div style={noPhotosStyle}>No photos</div>}
+      ? <img src={photos[0].getUrl()} alt="" height="50rem" style={photoStyles} />
+      : <div style={noPhotosStyle}>No photos</div>}
   </div>
 );
 
